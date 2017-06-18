47694cafe1b3fa63e40389347e3a6a74cfd91678
mitcheguevara <mitcheguevara@Mitches-MacBook-Air.local>
2017-06-18 19:44:56 -0400

    Missing ;

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,16 @@
-  console.log("Up and running");
   var cards = ["queen","queen","king","king"];
   var cardsInPlay = [];
   
   var cardOne = cards[0]; 
-  var cardTwo = cards [3];
+  var cardTwo = cards [1];
 
   cardsInPlay.push("cardOne", "cardTwo");
  
- if (cardsInPlay.lenght === 2) {
- 	if (cardsInPlay [0] === cardsInPlay [1]) {
- 		alert("You found a match")
+ if (cardsInPlay.length === 2) {
+ 	if (cardsInPlay [0] === cardsInPlay [0]) {
+ 		alert("You found a match");
  	}	else {
- 		alert ("Sorry, try again.")
+ 		alert ("Sorry, try again.");
  	}
  }
   console.log();
