1860d58ad62813273b540399dea348ae738a3903
mitcheguevara <mitcheguevara@Mitches-MacBook-Air.local>
2017-06-18 19:20:19 -0400

    unit8

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,19 @@
-  var cardOne = "queen";
-  var cardTwo = "queen";
-  var cardThree = "king"; 
-  var cardFour = "king";
-  console.log("User flipped " + cardOne);
+  console.log("Up and running");
+  var cards = ["queen","queen","king","king"];
+  var cardsInPlay = [];
+  
+  var cardOne = cards[0]; 
+  var cardTwo = cards [3];
+
+  cardsInPlay.push("cardOne", "cardTwo");
+ 
+ if (cardsInPlay.lenght === 2) {
+ 	if (cardsInPlay [0] === cardsInPlay [1]) {
+ 		alert("You found a match")
+ 	}	else {
+ 		alert ("Sorry, try again.")
+ 	}
+ }
+  console.log();
+  console.log("User flipped" + cardOne);
+  console.log("User flipped" + cardTwo);
