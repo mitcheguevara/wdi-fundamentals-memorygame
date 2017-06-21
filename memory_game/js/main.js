9889b22391911cd1b800fc5048f3524818bf0e71
mitcheguevara <mitcheguevara@Mitches-MacBook-Air.local>
2017-06-20 21:33:11 -0400

    gah

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,68 @@
-  var cards = ["queen","queen","king","king"];
+  var cards = [
+{
+  rank: 'queen',
+  suit: 'hearts'
+  cardImage: "images/queen-of-hearts.png"
+},
+{
+  rank: 'queen',
+  suit: 'diamonds',
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: 'king',
+  suit: 'hearts',
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: 'king',
+  suit: 'diamonds',
+  cardImage: "images/king-of-diamonds.png"
+}
+];
   var cardsInPlay = [];
-  
-  var cardOne = cards[0]; 
-  var cardTwo = cards [1];
+  var cardId = cards [0,1,2,3];
+ /* 
+  var checkForMatch = function() {
+	if(cardsInPlay [0] === cardsInPlay [1]) {
+		console.log("You've found a match!");
+	}  else {
+		console.log("Sorry,try again.");
+	}
+};
+  var flipCard = function (cardId){
+  	console.log("User flipped" + cards[cardId]);
+  if(cardsInPlay.length === 3) {
+  	if (cardsInPlay [0] === cardsInPlay[1]){
+  		 alert("You've found a match!");
+  	} else {
+  		alert("Sorry, try again.");
+  	}
+  }
+};
+*/
+var checkForMatch = function(cardId) {
+	if(cardsInPlay.length === 2)	{
+	 	if (cardsInPlay[0] === cardsInPlay[1]) {
+	      	console.log("You found a match!");
+	      	alert("You found a match!");
+  		} else {
+  			console.log("Sorry, try again.");
+     		 alert("Sorry, try again.");
+     		 
+     		 cardsInPlay = [];
+  		}
+  	}
+};
 
-  cardsInPlay.push("cardOne", "cardTwo");
+var flipCard = function() {
+	console.log('this', this)
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push([cardId].rank);
+	checkForMatch(cardId);
+  console.log(cardImage);
+  console.log(suit);
+};
+
  
- if (cardsInPlay.length === 2) {
- 	if (cardsInPlay [0] === cardsInPlay [0]) {
- 		alert("You found a match");
- 	}	else {
- 		alert ("Sorry, try again.");
- 	}
- }
-  console.log();
-  console.log("User flipped" + cardOne);
-  console.log("User flipped" + cardTwo);
+
