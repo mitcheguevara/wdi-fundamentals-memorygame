e702eda26727c32454ff8bbd297c37c89e9fb2ef
Mitche Guevara <mitche.guevara@gmail.com>
2017-06-25 08:56:15 -0400

    unit-11

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,24 +22,26 @@
 ];
 var cardsInPlay = [];
 var checkForMatch = function() {
-    if (cardsInPlay.length === 2);	
-	 	if (cardsInPlay[0] === cardsInPlay[1]) {
+    if (cardsInPlay.length === 2) 
+    if (cardsInPlay[0] === cardsInPlay[1]) {
 	   alert("You found a match!");
   		} 
     else {
      		 alert("Sorry, try again.");
   		}
 };
-checkForMatch();
+
 
 var flipCard = function() {
 	var cardId = this.getAttribute('data-id');
   console.log("User flipped" + " " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
   cardsInPlay.push(cards[cardId].rank);
   this.setAttribute('src',cards[cardId].cardImage);
   if (cardsInPlay.length === 2);
+  checkForMatch();
 };	
 var createBoard = function(){
     for (var i = 0; i < cards.length; i++){
